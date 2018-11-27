d528add3113703f03cd328a15adad520f09b62c8
ocastrejon <ocastrejon@ucol.mx>
2018-11-27 18:37:04 +0000

    Eliminar salas #3

--- eliminarSala.php
+++ eliminarSala.php
@@ -1,14 +1,16 @@
 <?php session_start(); 
 //datos para establecer la conexion con la base de mysql.
-require "cfg/conexion.php";
+//require "cfg/conexion.php";
+
+$conn = mysqli_connect("localhost", "root", "", "salas");
 
 // verificamos si se han enviado ya las variables necesarias.
 if (isset($_GET["id"])) {
     $id = $_GET["id"];
 
 
-                $query = 'DELETE  SALA_REMOTA WHERE idsala_remota='.$id;
-                mysql_query($query) or die(mysql_error());
+                $query = 'DELETE FROM SALA_REMOTA WHERE id_sala_remota='.$id;
+                mysqli_query($conn, $query) or die(mysqli_error($conn));
 }
 
    header('Location: index.php');
